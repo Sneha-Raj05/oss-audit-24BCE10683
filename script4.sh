13bc9e432ef614c35982905b7a954d837c4e38fa
Sneha Raj <sneharaj211979@gmail.com>
2026-03-31 18:52:04 +0530

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Script 4: Log File Analyzer
+# Author: Sneha Raj| Course: Open Source Software
+# Usage: ./log_analyzer.sh /var/log/syslog
+
+LOGFILE=$1
+KEYWORD=${2:-"error"} # Default keyword is 'error'
+COUNT=0
+
+# Check if the log file exists
+if [ ! -f "$LOGFILE" ]; then
+    echo "Error: File $LOGFILE not found."
+    exit 1
+fi
+
+# Use a while-read loop to process the file line by line
+while IFS= read -r LINE; do
+    if echo "$LINE" | grep -iq "$KEYWORD"; then
+        COUNT=$((COUNT + 1))
+    fi
+done < "$LOGFILE"
+
+echo "Keyword '$KEYWORD' found $COUNT times in $LOGFILE"
+
+# Display the last 5 matching lines
+echo "Last 5 matches:"
+grep -i "$KEYWORD" "$LOGFILE" | tail -n 5
+
